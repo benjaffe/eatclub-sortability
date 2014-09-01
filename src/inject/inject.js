6863a8cf46971a5503a670b2edd3e3d71e911db1
Ben Jaffe <jaffe.ben@gmail.com>
2014-09-01 10:48:22 -0400

    MutationObservers ftw!

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,11 +3,40 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
-		var vList = document.querySelectorAll('.menu-item [data-content^="Ve"]');
-        vList = Array.prototype.slice.call(vList);
-        vList = vList.map(function(v){
-            return v.parentNode.parentNode.parentNode.parentNode;
+
+
+		myeatclubPlugin.init();
+
+	}
+	}, 10);
+});
+
+var myeatclubPlugin = (function() {
+
+    function init() {
+        sort();
+
+        //init MutationObserver
+        var target = document.getElementById('menu-main');
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mut){
+                if (mut.addedNodes.length > 1) {
+                    sort();
+                }
+            });
         });
+        observer.observe(target, {childList:true});
+    }
+
+    function sort() {
+        var vList = document.querySelectorAll('.menu-item [data-content^="Ve"]');
+        vList = Array.prototype.slice.call(vList);
+
+        // get the item we want to sort
+        vList = vList.map(function(v){
+            return v.parentNode.parentNode.parentNode.parentNode; //TODO: Make this more robust
+        });
+
         vList.forEach(function(v) {
             var parent = v.parentNode;
             var first = parent.children[0];
@@ -18,7 +47,9 @@ chrome.extension.sendMessage({}, function(response) {
                 elem.style.marginRight = '';
             });
         });
+    }
 
-	}
-	}, 10);
-});
+    return {
+        init: init
+    };
+})();
