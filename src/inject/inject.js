eaf0dc1725838e90b380c5c2e66f5b91b3e384fb
Ben Jaffe <jaffe.ben@gmail.com>
2014-09-01 12:33:47 -0400

    broken stuff

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -11,6 +11,8 @@ chrome.extension.sendMessage({}, function(response) {
 	}, 10);
 });
 
+
+
 var myeatclubPlugin = (function() {
 
     function init() {
@@ -40,7 +42,8 @@ var myeatclubPlugin = (function() {
             {name:'Nuts',url:'nuts'},
             {name:'Spicy',url:'spicy'},
             {name:'Big-Portion',url:'big_portion'},
-            {name:'Low-Carb',url:'low_carb'}
+            {name:'Low-Carb',url:'low_carb'},
+            {name:'Paleo',url:'paleo'}
         ].map(function(obj){
             return '<li class="icon-sortable">' + obj.name + '</li>';
         }).join('');
@@ -48,28 +51,102 @@ var myeatclubPlugin = (function() {
         liElem.innerHTML = htmlStr;
     }
 
+    var sortOrder = ['vegan','vegetarian','big_portion'].reverse();
+    var listItems;
+
     function sort() {
-        var vList = document.querySelectorAll('.menu-item [data-content^="Ve"]');
-        vList = Array.prototype.slice.call(vList);
+        listItems = Array.prototype.slice.call(document.querySelectorAll('.menu-item'));
+        sortOrder.forEach(sortBy);
+    }
+    function sortBy(sortParam) {
+        // alert();
+        console.log('sorting by ' + sortParam);
 
-        // get the item we want to sort
-        vList = vList.map(function(v){
-            return v.parentNode.parentNode.parentNode.parentNode; //TODO: Make this more robust
+        listItems = listItems.mergeSort(function(item){
+            if (item.querySelector('[src*="'+sortParam+'"]')) {
+                console.log('found ' + sortParam);
+                return 0;
+            } else {
+                return 1;
+            }
         });
+        updateListItemOrder();
+    }
 
-        vList.forEach(function(v) {
-            var parent = v.parentNode;
-            var first = parent.children[0];
-            parent.insertBefore(v, first);
-
-            //remove manually-set margin-right
-            Array.prototype.slice.call(parent.children).forEach(function(elem){
-                elem.style.marginRight = '';
-            });
+    function updateListItemOrder() {
+        console.log('updating list',listItems.map(function(item){return item.querySelectorAll('.popover-target')}));
+        var parent = document.getElementById('menu-main');
+        console.log(listItems);
+        listItems.forEach(function(item) {
+            parent.appendChild(item);
         });
     }
 
     return {
         init: init
     };
+})();
+
+
+// Add stable merge sort to Array and jQuery prototypes
+// Note: We wrap it in a closure so it doesn't pollute the global
+//       namespace, but we don't put it in $(document).ready, since it's
+//       not dependent on the DOM
+(function() {
+
+  // expose to Array and jQuery
+  Array.prototype.mergeSort = mergeSort;
+
+  function mergeSort(compare) {
+
+    var length = this.length,
+        middle = Math.floor(length / 2);
+
+    if (!compare) {
+      compare = function(left, right) {
+        if (left < right)
+          return -1;
+        if (left == right)
+          return 0;
+        else
+          return 1;
+      };
+    }
+
+    if (length < 2)
+      return this;
+
+    return merge(
+      this.slice(0, middle).mergeSort(compare),
+      this.slice(middle, length).mergeSort(compare),
+      compare
+    );
+  }
+
+  function merge(left, right, compare) {
+
+    var result = [];
+
+    while (left.length > 0 || right.length > 0) {
+      if (left.length > 0 && right.length > 0) {
+        if (compare(left[0], right[0]) <= 0) {
+          result.push(left[0]);
+          left = left.slice(1);
+        }
+        else {
+          result.push(right[0]);
+          right = right.slice(1);
+        }
+      }
+      else if (left.length > 0) {
+        result.push(left[0]);
+        left = left.slice(1);
+      }
+      else if (right.length > 0) {
+        result.push(right[0]);
+        right = right.slice(1);
+      }
+    }
+    return result;
+  }
 })();
